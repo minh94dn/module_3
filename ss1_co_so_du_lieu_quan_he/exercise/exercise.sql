45dc953bf3984939ef63630727ee7148f6367c2e
minh94dn <grakk2405@gmail.com>
2022-11-14 15:43:43 +0700

    nop bai tap ss1

---
+++ ss1_co_so_du_lieu_quan_he/exercise/exercise.sql
@@ -0,0 +1,13 @@
+create database `class-management`;
+create table class(
+id int primary key,
+`name` varchar(45)
+ );
+create table teteacheracher(
+id int primary key,
+`name` varchar(45),
+age int,
+country varchar(45)
+ );
+select * from class;
+select * from teacher;
