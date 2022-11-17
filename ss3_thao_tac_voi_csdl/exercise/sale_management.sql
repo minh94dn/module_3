d30a3cd91b836786a1800fb608d75f85b7f9a11d
minh94dn <grakk2405@gmail.com>
2022-11-17 11:46:48 +0700

    update ss3

--- ss3_thao_tac_voi_csdl/exercise/sale_management.sql
+++ ss3_thao_tac_voi_csdl/exercise/sale_management.sql
@@ -1,10 +1,12 @@
-create database if not exists quan_ly_ban_hang;
-use quan_ly_ban_hang;
+create database if not exists sale_management;
+use sale_management;
+
 create table customer(
   c_id int primary key, 
   `name` varchar(25), 
   c_age tinytext
 );
+
 create table `order`(
   o_id int primary key, 
   c_id int, 
@@ -12,11 +14,13 @@ create table `order`(
   o_total_price int, 
   foreign key(c_id) references customer(c_id)
 );
+
 create table product(
   p_id int primary key, 
   p_name varchar(25), 
   p_price int
 );
+
 create table order_detail(
   o_id int, 
   p_id int, 
@@ -24,16 +28,19 @@ create table order_detail(
   foreign key(p_id) references product(p_id), 
   foreign key(o_id) references `order`(o_id)
 );
+
 insert into customer 
 values 
   (1, 'Minh Quan', 10), 
   (2, 'Ngoc Oanh', 20), 
   (3, 'Hong Ha', 50);
+  
 insert into `order` 
 values 
   (1, 1, '2006/3/21', null), 
   (2, 2, '2006/3/23', null), 
   (3, 1, '2006/3/16', null);
+  
 insert into product 
 values 
   (1, 'May Giat', 3), 
@@ -41,6 +48,7 @@ values
   (3, 'Dieu Hoa', 7), 
   (4, 'Quat', 1), 
   (5, 'Bep Dien', 2);
+  
 insert into order_detail 
 values 
   (1, 1, 3), 
@@ -50,10 +58,14 @@ values
   (3, 1, 8), 
   (2, 5, 4), 
   (2, 3, 3);
+  
+  -- Hiển thị các thông tin  gồm oID, oDate, oPrice của tất cả các hóa đơn trong bảng Order
 select 
   * 
 from 
   `order`;
+  
+  -- Hiển thị danh sách các khách hàng đã mua hàng, và danh sách sản phẩm được mua bởi các khách
 select 
   c.`name`, 
   p.p_name 
@@ -62,6 +74,8 @@ from
   inner join `order` as o on o.o_id = od.o_id 
   inner join customer as c on c.c_id = o.c_id 
   inner join product as p on p.p_id = od.p_id;
+  
+  -- Hiển thị tên những khách hàng không mua bất kỳ một sản phẩm nào
 select 
   c.`name` 
 from 
@@ -69,6 +83,8 @@ from
   left join `order` as o on c.c_id = o.c_id 
 where 
   o.o_id is null;
+  
+  -- Hiển thị mã hóa đơn, ngày bán và giá tiền của từng hóa đơn (giá một hóa đơn được tính bằng tổng giá bán của từng loại mặt hàng xuất hiện trong hóa đơn. Giá bán của từng loại được tính = odQTY*pPrice)
 select 
   o.o_id, 
   o.o_date, 
--- ss3_thao_tac_voi_csdl/exercise/student_management.sql
+++ ss3_thao_tac_voi_csdl/exercise/student_management.sql
@@ -1,11 +1,13 @@
-create database if not exists quan_ly_sinh_vien;
-use quan_ly_sinh_vien;
+create database if not exists student_management;
+use student_management;
+
 create table class(
   class_id int auto_increment primary key, 
   class_name varchar(60) not null, 
   start_date datetime not null, 
   `status` bit
 );
+
 create table student(
   student_id int auto_increment primary key, 
   student_name varchar(30) not null, 
@@ -15,12 +17,14 @@ create table student(
   class_id int, 
   foreign key(class_id) references class(class_id)
 );
+
 create table `subject`(
   sub_id int auto_increment primary key, 
   sub_name varchar(30) not null, 
   credit tinyint not null default 1 check (credit >= 1), 
   `status` bit default 1
 );
+
 create table mark(
   mark_id int auto_increment primary key, 
   sub_id int, 
@@ -28,18 +32,20 @@ create table mark(
   mark float default 0 check (
     mark between 0 
     and 100
-  ), 
+  ),
   exam_times tinyint default 1, 
   unique(sub_id, student_id), 
   foreign key(sub_id) references `subject`(sub_id), 
   foreign key(student_id) references student(student_id)
 );
+
 insert into class 
 values 
   (1, 'A1', '2008-12-20', 1), 
   (2, 'A2', '2008-12-22', 1), 
   (3, 'B3', current_date, 0);
-insert into student(
+  
+  insert into student(
   student_name, address, phone, `status`, 
   class_id
 ) 
@@ -48,23 +54,27 @@ values
     'Hung', 'Ha Noi', '0912113113', 1, 
     1
   );
+  
 insert into student(
   student_name, address, `status`, class_id
 ) 
 values 
   ('Hoa', 'Hai Phong', 1, 1);
+  
 insert into student(
   student_name, address, phone, `status`, 
   class_id
 ) 
 values 
   ('Manh', 'HCM', '0123123123', 0, 2);
+  
 insert into `subject` 
 values 
   (1, 'CF', 5, 1), 
   (2, 'C', 6, 1), 
   (3, 'HDJ', 5, 1), 
   (4, 'RDBMS', 10, 1);
+  
 insert into mark (
   sub_id, student_id, mark, exam_times
 ) 
@@ -72,18 +82,24 @@ values
   (1, 1, 8, 1), 
   (1, 2, 10, 2), 
   (2, 1, 12, 1);
+  
+  -- Hiển thị tất cả các sinh viên có tên bắt đầu bảng ký tự ‘h’.
 select 
   * 
 from 
   student 
 where 
   student_name like "h%";
+  
+  -- Hiển thị các thông tin lớp học có thời gian bắt đầu vào tháng 12.
 select 
   * 
 from 
   class 
 where 
   start_date like "%-12-%";
+  
+-- Hiển thị tất cả các thông tin môn học có credit trong khoảng từ 3-5.
 select 
   * 
 from 
@@ -91,6 +107,8 @@ from
 where 
   credit >= 3 
   and credit <= 5;
+  
+ -- Thay đổi mã lớp(ClassID) của sinh viên có tên ‘Hung’ là 2.
 set 
   SQL_SAFE_UPDATES = 0;
 update 
@@ -101,6 +119,8 @@ where
   student_name = 'Hung';
 set 
   SQL_SAFE_UPDATES = 1;
+  
+  -- Hiển thị các thông tin: StudentName, SubName, Mark. Dữ liệu sắp xếp theo điểm thi (mark) giảm dần. nếu trùng sắp theo tên tăng dần.
 select 
   s.student_name, 
   sub.sub_name, 
