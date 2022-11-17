e902289031e932e687465e407daa56d30c4ff12c
minh94dn <grakk2405@gmail.com>
2022-11-17 17:14:39 +0700

    nộp bài tập ss4

--- case_study/furama_query.sql
+++ case_study/furama_query.sql
@@ -1,1 +1,5 @@
 use furama;
+
+-- 2.	Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là một trong các ký tự “H”, “T” hoặc “K” và có tối đa 15 kí tự.
+select * from nhan_vien as nv where substring_index(nv.ho_ten, ' ',-1) like 'H%' or substring_index(nv.ho_ten, ' ',-1) like 'T%' or substring_index(nv.ho_ten, ' ',-1) like 'K%';
+
